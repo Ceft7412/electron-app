cc9dc9fadfa1301a4da04dfdc3e58873ee7526be
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-08-11 14:27:29 +0800

    @add Imported  2 modules using the CommonJS module syntax. Then instantiating the BrowserWindow and called the createWindow function when the app is ready (i.e. app.whenReady())..

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
+// Imported 2 modules using CommonJS module syntax.
+// app controls the application's event lifecycle.
+// BrowserWindow creates and manages the app windows.
+
 const { app, BrowserWindow } = require("electron");
 
+
+
 const createWindow = () => {
-  const win = BrowserWindow({
+  const win = new BrowserWindow({
     width: 800,
     height: 600,
   });
   win.loadFile("index.html");
 };
 
+// BrowserWindows can only be created after the app module's ready event is fired.
 app.whenReady().then(() => {
   createWindow();
 });
